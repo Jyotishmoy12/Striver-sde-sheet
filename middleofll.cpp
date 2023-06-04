1b540cee4e052eb7fb3e9808af9d6adcc31435c8
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2023-06-04 20:13:24 +0530

    Create middleofll.cpp

---
+++ middleofll.cpp
@@ -0,0 +1,14 @@
+Node *findMiddle(Node *head) {
+    if(head==NULL && head->next==NULL) return head;
+    Node*slow=head;
+    Node*fast=head->next;
+    while(fast!=NULL){
+        fast=fast->next;
+        if(fast!=NULL){
+            fast=fast->next;
+        }
+        slow=slow->next;
+    }
+    return slow;
+}
+
