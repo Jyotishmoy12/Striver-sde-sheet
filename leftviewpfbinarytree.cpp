cb64586abd6e714159bfc705f8875803ad35a32e
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2023-06-16 19:31:41 +0530

    Create leftviewpfbinarytree.cpp

---
+++ leftviewpfbinarytree.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+void leftView(TreeNode<int> *root, int level, vector<int>&res){
+    if(root==NULL) return ;
+    if(level==res.size()) res.push_back(root->data);
+    leftView(root->left, level+1, res);
+    leftView(root->right, level+1, res);
+}
+vector<int> getLeftView(TreeNode<int> *root)
+{
+   vector<int>res;
+   leftView(root, 0, res);
+   return res;
+}
