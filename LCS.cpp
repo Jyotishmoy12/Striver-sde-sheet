45e1efc0bd03dfb098188f5933c9dac72f68ea64
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2023-06-28 11:16:32 +0530

    Create LCS.cpp

---
+++ LCS.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+int lcs(string s, string t)
+{
+	int n=s.size();
+	int m=t.size();
+	vector<vector<int>>dp(n+1, vector<int>(m+1,-1));
+	for(int i=0;i<=n;i++) dp[i][0]=0;
+	for(int i=0;i<=m;i++) dp[0][i]=0;
+	for(int ind1=1;ind1<=n;ind1++){
+		for(int ind2=1;ind2<=m;ind2++){
+			if(s[ind1-1]==t[ind2-1]){
+				dp[ind1][ind2]=1+dp[ind1-1][ind2-1];
+			}
+			else{
+				dp[ind1][ind2]=0+max(dp[ind1-1][ind2], dp[ind1][ind2-1]);
+			}
+		}
+	}
+	return dp[n][m];
+}
